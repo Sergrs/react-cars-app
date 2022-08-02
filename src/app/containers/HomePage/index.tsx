8f932a5e4e7ee28abe9d79c47d2f5ee7b7ab58c8
Serg <sergeyrs@bk.ru>
2022-08-02 22:25:49 +0700

    initial commit

---
+++ src/app/containers/HomePage/index.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styled from "styled-components";
+import tw from "twin.macro";
+import Navbar from "../../components/navbar";
+import TopSection from "./topSection";
+
+const PageContaier = styled.div`
+  ${tw`
+        flex
+        flex-col
+        w-full
+        h-full
+        items-center
+        overflow-x-hidden
+    `}
+`;
+export function HomePage() {
+  return (
+    <PageContaier>
+      <Navbar />
+      <TopSection />
+    </PageContaier>
+  );
+}
